945285c7f075e87134001023db8a59245621a397
namsoon00 <namsoon00@users.noreply.github.com>
2026-06-16 08:44:37 +0900

    Deploy MarketFlow API settings build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1829028216" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "435525479" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
